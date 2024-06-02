15e52f6edbf5661fb72ed748569e645c9682fc5e
khaooat. <alongkorn04445@gmail.com>
2024-06-02 19:22:15 +0700

    last

--- src/app/dialog-movie/dialog-movie.component.ts
+++ src/app/dialog-movie/dialog-movie.component.ts
@@ -1,5 +1,11 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Inject} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface form_detail{
+  typeChair : string,
+  chair : string,
+  payMent : string,
+}
 
 @Component({
   selector: 'app-dialog-movie',
@@ -8,10 +14,35 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class DialogMovieComponent {
 
+  formDetail: form_detail = {
+    typeChair:'',
+    chair: '',
+    payMent:'',
+  };
+
+  formInvalid: boolean = false;
+
 
   constructor(
     public dialogRef: MatDialogRef<DialogMovieComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ){}
+
+  onSubmit(){
+    if (this.checkFormInvalid()) {
+      this.formInvalid = true;
+      return;
+    } else {
+      this.dialogRef.close(this.formDetail);
+    }
+  }
+
+  checkFormInvalid(): boolean {
+    return (
+      (this.formDetail.typeChair?.trim() === '' || this.formDetail.typeChair == null) ||
+      (this.formDetail.chair?.trim() === '' || this.formDetail.chair == null) ||
+      (this.formDetail.payMent?.trim() === '' || this.formDetail.payMent == null)
+    );
+  }
 
 }
