d27e4247131e40b3b0ca42188e8cb949575ce280
khaooat. <alongkorn04445@gmail.com>
2024-06-03 03:24:57 +0700

    csacsa

--- src/app/dialog-movie/dialog-movie.component.ts
+++ src/app/dialog-movie/dialog-movie.component.ts
@@ -55,7 +55,7 @@ export class DialogMovieComponent implements OnInit {
       };
 
       // ส่งข้อมูลกลับไปยังเซิร์ฟเวอร์
-      this.http.post('https://your-ngrok-url.ngrok-free.app/optionspostback', data).subscribe(
+      this.http.post('https://bbcd-2001-fb1-c4-a1b5-612b-ba62-2e7b-12e6.ngrok-free.app/optionspostback', data).subscribe(
         response => {
           console.log('Booking information sent successfully', response);
           // Use Messenger API to send a message back to the user
@@ -86,7 +86,7 @@ export class DialogMovieComponent implements OnInit {
   }
 
   sendConfirmationMessage(psid: string, message: string) {
-    const PAGE_ACCESS_TOKEN = 'EAAGrx9Dtmf4BOw9EirXaW2t4xeZB8MaiTUZBU7d7COdeGTp8pZCMSsxdaKY6e2NUhKlST9i7ZBNQDRKRWz83F9kvfGfUeGQ2sLvR6mk5bKbTX6NWN5ShCowkcbBKOJk6JXBZCrrrZCTZCZBUiALwQqPWpYKBRrgV4FP8OczjPZBYlFZAcffH23HOSUdHiYyx2M7NK4ygZDZD';
+    const PAGE_ACCESS_TOKEN = 'EAAGrx9Dtmf4BOxSkwkwnLyXd5nZAIgyWEOZCPXFDBAe3LdqYCUltTlbyZCgZAEC2feYeZCVUGPYxZB24V5Y6X62BKUmIU7zONIleGDZCLYjhqZCwAVLhVa40Xy9vQ4USvZCaZCZA43H1LLZCLP0bZClEzGvTgfaJdEEEZBXnZC3OQZCxMXZAJ3YSIhW0x98s2nC5j8yh6by2nMo2j2ZAZAe8yTu3n7vxlDz1YW2Id4ZD';
     const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
     const body = {
       recipient: { id: psid },
